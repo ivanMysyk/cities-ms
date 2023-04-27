73a9b6e734455a274b8f3c8b42d28bac05d4cc09
Ivan Mysyk <ivan.mysyk04@gmail.com>
2023-04-27 18:52:46 +0300

    init commit

---
+++ src/main/java/com/test/task/kuehne/cities/TestTaskKuehneApplication.java
@@ -0,0 +1,13 @@
+package com.test.task.kuehne.cities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskKuehneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskKuehneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/task/kuehne/cities/TestTaskKuehneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.task.kuehne.cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskKuehneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
